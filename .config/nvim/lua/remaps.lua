ffea0183f9be9bf52ff399f7d5ebb00a556f11a7
Luis Figueroa <luis_figueroa_morales@yahoo.com>
2023-02-03 09:40:48 -0500

    added custom latex snippets and fix remaps

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 -- enviroment
 vim.keymap.set({'n', 'i', 'v'}, '<F6>', '<esc>:setlocal spell! spelllang=es,en,fr')
 vim.keymap.set({'n', 'i', 'v'}, '<F3>', '<cmd>set rnu! | set nu!<cr>', {silent = true})
-vim.keymap.set('n', '<leader>r', '<cmd>set registers<cr>', { expr = true } )
+vim.keymap.set('n', '<leader>R', '<cmd>registers<cr>')
 vim.keymap.set('n', '<leader>i', '<cmd>set ignorecase!<cr>')
 vim.keymap.set('n', '<leader>h', '<cmd>set hlsearch!<cr>')
 vim.keymap.set('n', '<leader>cd', '<cmd>cd %:h<cr>')
